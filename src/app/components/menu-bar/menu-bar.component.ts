ba8d894d27f4fc42425a9a5a20ff63cf0ea8b662
francelino <fmarcilio1989@gmail.com>
2022-10-03 10:25:15 -0300

    feat dark theme

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.css']
+  styleUrls: ['./menu-bar.component.css'],
 })
 export class MenuBarComponent implements OnInit {
+  themeDark: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggle() {
+    this.themeDark = !this.themeDark;
+    document.body.classList.toggle('dark-theme');
   }
-
 }
